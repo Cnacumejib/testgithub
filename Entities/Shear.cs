16f866989e540b4ecb4e92e0ae79f6b9aa6f9877
Aleksander <burunduk73@mail.ru>
2018-10-09 01:18:56 +0400

    shear

---
+++ Entities/Shear.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Entities
+{
+    class Shear
+    {
+        int x = 0;
+        int y = 0;
+        int quadrant = 1;
+        int type = 0;
+
+        public Shear()
+        {
+        }
+
+        public bool IsInside()
+        {
+            return false;
+        }
+
+        public void Draw(ref Bitmap bitmap, Color parentColor)
+        {
+            if(parentColor==Color.Blue)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+        public void SetSize(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void SetType(int type)
+        {
+            this.type = type;
+        }
+
+        public void SetQuadrant(int quadrant)
+        {
+            this.quadrant = ((quadrant-1) % 4)+1;
+        }
+    }
+}
